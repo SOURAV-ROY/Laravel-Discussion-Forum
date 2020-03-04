be5da2bb5dfe62ac4dfe1f68fee4b94eec628e6d
SOURAV ROY <souravroy.info13@gmail.com>
2020-03-05 00:19:34 +0600

    Auth Control Redirect To DISCUSSIONS � ✔

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::DISCUSSIONS;
 
     /**
      * Create a new controller instance.
